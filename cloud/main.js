be30359d909892de5d04a08c3951c2b4d982050c
Roger Hu <roger.hu@gmail.com>
2016-11-03 00:16:52 -0700

    Get stuff working for initial channel

--- cloud/main.js
+++ cloud/main.js
@@ -1,38 +1,32 @@
-// Android push test
-Parse.Cloud.define('pushChannelTest', function(request, response) {
+// See http://parseplatform.github.io/docs/js/guide/
+// https://parseplatform.github.io/docs/js/guide/#query-constraints
+Parse.Cloud.define('pushToChannel', function(request, response) {
 
-  // request has 2 parameters: params passed by the client and the authorized user
   var params = request.params;
-  var user = request.user;
 
-  // To be used with:
-  // https://github.com/codepath/ParsePushNotificationExample
-  // See https://github.com/codepath/ParsePushNotificationExample/blob/master/app/src/main/java/com/test/MyCustomReceiver.java
+  var channel = params.channel;
+
+  if (!channel) {
+     response.error('must provide a channel');
+  }
+
   var customData = params.customData;
-  var launch = params.launch;
-  var broadcast = params.broadcast;
 
   // use to custom tweak whatever payload you wish to send
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo("deviceType", "android");
+  var channels = [channel];
 
   var payload = {};
 
   if (customData) {
-      payload.customdata = customData;
-  }
-  else if (launch) {
-      payload.launch = launch;
-  }
-  else if (broadcast) {
-      payload.broadcast = broadcast;
+      payload = JSON.parse(customData);
   }
 
   // Note that useMasterKey is necessary for Push notifications to succeed.
 
   Parse.Push.send({
-  where: pushQuery,      // for sending to a specific channel
-  data: payload,
+  channels: channels,
+  // Parse.Push requires a dictionary, not a string.
+  data: {"customData": customData},
   }, { success: function() {
      console.log("#### PUSH OK");
   }, error: function(error) {
@@ -41,31 +35,3 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
 
   response.success('success');
 });
-
-// iOS push testing
-Parse.Cloud.define("iosPushTest", function(request, response) {
-
-  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
-  var params = request.params;
-  var user = request.user;
-
-  // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
-  var messageText = params.text;
-
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
-
-  Parse.Push.send({
-    where: pushQuery, // Set our Installation query                                                                                                                                                              
-    data: {
-      alert: "Message: " + messageText
-    }
-  }, { success: function() {
-      console.log("#### PUSH OK");
-  }, error: function(error) {
-      console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
-  response.success('success');
-});
-
