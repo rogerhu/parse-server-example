783c013c370e514e67d9ff7971263d878b17bedb
Roger Hu <roger.hu@gmail.com>
2017-03-23 23:44:59 -0700

    Forgot require('path')

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
