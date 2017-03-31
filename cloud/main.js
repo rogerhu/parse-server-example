324c75e9b48c5848c0e0ed389feed2072b4ff603
Roger Hu <roger.hu@gmail.com>
2017-03-30 23:45:36 -0700

    Respond to sender

--- cloud/main.js
+++ cloud/main.js
@@ -39,3 +39,28 @@ Parse.Cloud.define('pushToChannel', function(request, response) {
 
   response.success('success');
 });
+
+Parse.Cloud.define('pingReply', function(request, response) {
+  var params = request.params;
+
+  var sender = params.sender;
+
+  if (!sender) {
+    response.error("No response!")
+  }
+
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo("objectId", sender);
+
+  Parse.Push.send({
+  where: query,
+  // Parse.Push requires a dictionary, not a string.
+  data: {"customData": "Reply!!"},
+  }, { success: function() {
+     console.log("#### PUSH OK");
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
+});
